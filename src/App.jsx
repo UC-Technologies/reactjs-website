5f161f54c68872f1b8811667dcb15dd7a7188667
hereisadi <hereisadi@proton.me>
2024-03-06 21:53:52 +0530

    ADD:tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-
-
-
-
 const App = () => {
   return (
     <main>
-      This is home page
+      <h1 className="text-4xl font-bold underline text-red-600">Hello world!</h1>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
